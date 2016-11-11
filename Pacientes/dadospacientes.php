abf5ddf2e4161d2abd589d55f692feeb6eda26c2
carolvicente <carolvicente.tec@gmail.com>
2016-11-11 15:37:33 -0200

    Visualização de Dados
    Esta funcionando

--- Pacientes/dadospacientes.php
+++ Pacientes/dadospacientes.php
@@ -1,42 +1,39 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="CSS/estilo.css" />
-		<title> Pacientes</title>
-		
-	</head>
-	
-	<body>
-	
-		<div id="dados">
-			<table id="tabelaPacientes">
-				<tr id="cabecalho" class="linha">
-					<td>Nome</td>
-					<td>CPF</td>
-				</tr>
-				<tr>
-					<td class="dado">Vitor Artoni de Marcio</td>
-					<td class="dado">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;12345678910
-					<a href="detalhes.php" target=""> Detalhes</a></td>
-				</tr>
-				<tr>
-					<td class="dado">Mirelle Bueno</td>
-					<td class="dado">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;12312312312
-					<a href="detalhes.php" target=""> Detalhes</a></td>
-				</tr>
-				<tr>
-					<td class="dado">Carol Vicente</td>
-					<td class="dado">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;32132132132
-					<a href="detalhes.php" target=""> Detalhes</a></td>
-				</tr>
-				<tr>
-					<td class="dado">Otavio Pasarelli</td>
-					<td class="dado">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;98765432101
-					<a href="detalhes.php" target=""> Detalhes</a></td>
-				</tr>
-			</table>
-				
-		</div>
-	</body>
-	
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="CSS/estilo.css" />
+    <title>Dados do Paciente</title>
+</head>
+<body>
+
+    <div id="dados">
+<?php
+  
+    error_reporting(E_ALL ^ E_DEPRECATED);
+
+    require_once ('../scripts/conexao.php');
+    $query = "SELECT * FROM paciente";
+    $resultado = mysqli_query($conexao,$query);
+
+    if(mysqli_num_rows($resultado)>0)
+    {
+        echo "<table id='tabelaPacientes'><tr id='cabecalho' class='linha'><td>CPF</td><td>Nome</td></tr>";
+        while($row = mysqli_fetch_assoc($resultado))
+        {
+            echo "<tr><td class='dado'>".$row["cpf"]."</td><td class='dado'>".$row["nome"]."</td><td><form action='detalhes.php'>
+    <input type='submit' id=".$row["cpf"]." value='Ver detalhes' /></form></td></tr>";
+        }
+        echo "</table>";
+    }
+    else
+    {
+        echo "Não há pacientes cadastrados no sistema.";
+    }
+
+    mysqli_close($conexao);
+?>
+
+    </div>
+</body>
 </html>
