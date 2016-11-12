6b30c85a1ac2a107f82b70ff7f672cea11b72acc
carolvicente <carolvicente.tec@gmail.com>
2016-11-12 21:37:25 -0200

    Menu Adicional para médico

---
+++ Sistema/menuprincipalNormal.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"> 
+		<link rel="stylesheet" type="text/css" href="CSS/estilo.css" />
+		<title> Menu Principal</title>
+	</head>
+	
+	<body>
+			<div id="container">
+			
+				<div id="menu">
+					<h3> <a href="dadospacientes.php" target=""> <input type="button" id="botao_menu" value="Visualizar Dados"/> </a> </h3>
+				</div>
+				
+				<div id="conteudo">
+					<iframe id="Iframe" name="iframe_a"> </iframe>
+				</div>
+				
+			</div>
+	</body>
+	
+</html>
